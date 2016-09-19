ff8e6cea24caaba95712eb63783a2cb7c59b70b8
Avi Schwab <schwab@palantir.net>
2016-09-19 12:06:34 -0500

    Add additional conditions for header markup

--- features/bootstrap/MassMarkupContext.php
+++ features/bootstrap/MassMarkupContext.php
@@ -11,6 +11,11 @@ class MassMarkupContext extends MarkupContext
   public function iShouldSeeTheCorrectMarkupForTheHeader()
   {
       $this->assertRegionElement('.ma__header__skip-nav', 'header');
-
+      $this->assertRegionElement('.ma__header__backto', 'header');
+      $this->assertRegionElement('.ma__header__container > .ma__header__logo > .ma__site-logo > a', 'header');
+      $this->assertRegionElement('nav.ma__header__nav > .ma__header__button-container > .ma__header__back-button', 'header');
+      $this->assertRegionElement('nav.ma__header__nav > .ma__header__button-container > .ma__header__menu-button', 'header');
+      $this->assertRegionElement('nav.ma__header__nav > .ma__header__nav-container > .ma__header__main-nav > .ma__main-nav > ul.ma__main-nav__items', 'header');
+      $this->assertRegionElement('ul.ma__main-nav__items > li.ma__main-nav__item > a.ma__main-nav__top-link', 'header');
   }
 }
