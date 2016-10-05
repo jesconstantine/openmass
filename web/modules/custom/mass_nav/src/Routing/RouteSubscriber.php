946bcbd0a1a3ba1be544cee8ee9db678318bc1ab
Kelsey Bentham <bentham@palantir.net>
2016-10-05 12:18:48 -0500

    Alter the route for the subtopics rerorder tab so that it uses the admin theme.

---
+++ web/modules/custom/mass_nav/src/Routing/RouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\mass_nav\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+
+    if ($route = $collection->get('view.subtopics.subtopic_reorder')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+  }
+
+}
