279cc843cf86c440f4e951209432d41e73a9b0f7
Jes Constantine <jesconstantine@gmail.com>
2016-10-18 19:32:07 -0400

    Update formstack select elements to use Mayflower js select link

--- web/themes/custom/mass_theme/libraries/formstack/js/matchMayflower.js
+++ web/themes/custom/mass_theme/libraries/formstack/js/matchMayflower.js
@@ -7,6 +7,39 @@ $(function(){
       // $('.fsBody link').remove();
       // $('.fsBody style').remove();
 
+
+      // Update select elements to match Mayflower
+      $('select.fsField').each(function(el,i) {
+
+        var $this = $(this),
+        $label = $this.siblings( "label[for=" + $this.attr( "id" ) + "]" );
+
+        // add class to label
+        $label.addClass('ma__select-box__label');
+
+        // add class to field div container
+        $(this).parent().addClass('ma__feedback-form__type');
+
+        // wrap label + input with section.ma__select-box.js-dropdown
+        $this.add($label).wrapAll( '<section class="ma__select-box js-dropdown"/>' );
+
+        // 1. add classes to the select element
+        // 2. wrap select element with div.ma__select-box__field
+        $(this).addClass('ma__select-box__select js-dropdown-select')
+               .wrap('<div class="ma__select-box__field" />');
+
+        // get the text value of the first option (used below)
+        var firstOptionText = $(this).find('option').first().text();
+
+        // create the div.ma__select-box__link + child spans,
+        // this is what is rendered visually in place of the select
+        $(this).after('<div class="ma__select-box__link">'
+                      + '<span class="js-dropdown-link">'
+                      + firstOptionText
+                      + '</span><span class="ma__select-box__icon"></span></div>');
+
+      });
+
       /*
       // Map checkbox form elements to what Mayflower is expecting
        $('.fsOptionLabel > input[type="checkbox"]').each(function(el, i) {
@@ -45,12 +78,12 @@ $(function(){
 
       //   -- app specific overrides -- //
 
+      // HOMEPAGE -- //
       // Add appropriate class to homepage form > radio fieldset
-      $('#label46538067').addClass('ma__feedback-form__permission')
+      $('#label46538067').addClass('ma__feedback-form__permission');
 
-      // Map supporting .showMobile for file upload field to USWDS support text
-      // $('.showMobile')
-        // .addClass('usa-form-hint usa-additional_text');
+      // FEEDBACK FORM -- //
+
 
     // ---- end formstack reset ------ //
 
