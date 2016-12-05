b8c51926013fbd0fb05cc01b621724786fac832d
Jes Constantine <jesconstantine@gmail.com>
2016-12-05 14:31:24 -0500

    Make external link + interstitial js  pass eslint
    *Should* not be any functional changes in this commit

--- web/themes/custom/mass_theme/libraries/interstitial/js/external-links.js
+++ web/themes/custom/mass_theme/libraries/interstitial/js/external-links.js
@@ -1,13 +1,14 @@
-$(function(){
-  $('a[href]').click(function(e) {
+(function ($) {
+  'use strict';
+  $('a[href]').click(function (e) {
     var href = $(this).attr('href');
-    if(window.localStorage.noInterstitial && JSON.parse(window.localStorage.noInterstitial)) {
+    if (window.localStorage.noInterstitial && JSON.parse(window.localStorage.noInterstitial)) {
       return;
     }
     // TODO: This is not a valid test to ship. We should use a more sophisticated regular expression or something to cover all scenarios.
-    if(href.indexOf('mass.gov') == -1 || href.indexOf('/agencies/')) {
+    if (href.indexOf('mass.gov') === -1 || href.indexOf('/agencies/')) {
       e.preventDefault();
-      window.location = window.location.origin+"/leaving-pilot?continueURL="+this.href;
+      window.location = window.location.origin + '/leaving-pilot?continueURL=' + this.href;
     }
   });
-});
+})(jQuery);
--- web/themes/custom/mass_theme/libraries/interstitial/js/interstitial-page.js
+++ web/themes/custom/mass_theme/libraries/interstitial/js/interstitial-page.js
@@ -1,37 +1,41 @@
-$(function(){
-  console.info('interstitial page');
+(function ($) {
+  'use strict';
+  // console.info('interstitial page');
 
-  var queryString = (function(a) {
-    if (a == "") return {};
+  var queryString = (function (a) {
+    if (a === '') {
+      return {};
+    }
     var b = {};
-    for (var i = 0; i < a.length; ++i)
-    {
-        var p=a[i].split('=', 2);
-        if (p.length == 1)
-            b[p[0]] = "";
-        else
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    for (var i = 0; i < a.length; ++i) {
+      var p = a[i].split('=', 2);
+      if (p.length === 1) {
+        b[p[0]] = '';
+      }
+      else {
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
+      }
     }
     return b;
   })(window.location.search.substr(1).split('&'));
 
-  if(window.localStorage.noInterstitial) {
+  if (window.localStorage.noInterstitial) {
     $('#hide-transition-page').attr('checked', JSON.parse(window.localStorage.noInterstitial));
   }
 
 
-  $('#hide-transition-page').change(function(e) {
-    if(window.localStorage) {
+  $('#hide-transition-page').change(function (e) {
+    if (window.localStorage) {
       window.localStorage.noInterstitial = this.checked ? true : null;
     }
   });
 
-  $('.js-interstitial-button-continue').click(function() {
+  $('.js-interstitial-button-continue').click(function () {
     // Default to mass.gov if somehow there is no incoming query parameter
     window.location.href = queryString.continueURL || 'https://mass.gov';
   });
 
-  $('.js-interstitial-button-back').click(function() {
+  $('.js-interstitial-button-back').click(function () {
     window.history.back();
   });
-});
+})(jQuery);
