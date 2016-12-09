1165ffa65d79eab1ac64bc000d76bede49216e56
Rob Powell <rob.p.powell@gmail.com>
2016-12-09 18:00:23 -0500

    Update js after eslint

--- web/themes/custom/mass_theme/libraries/ajax_views_ext/js/ajaxViewsExt.js
+++ web/themes/custom/mass_theme/libraries/ajax_views_ext/js/ajaxViewsExt.js
@@ -3,15 +3,16 @@
  * Extends drupal view AJAX filtering functionality with accessible announcements.
  */
 (function ($, Drupal) {
+  'use strict';
 
   // @TODO: revisit and confirm this language.
-  var FILTERED_ANNOUNCEMENT = "New content loaded. Now displaying a new set of filtered items.";
+  var FILTERED_ANNOUNCEMENT = 'New content loaded. Now displaying a new set of filtered items.';
 
   Drupal.behaviors.ajaxViewsExt = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       // We hook off of the document-level view ajax event
       $(document).once('views-ajax').ajaxComplete(function (e, xhr, settings) {
-        xhr.done(function() {
+        xhr.done(function () {
           Drupal.announce(
             Drupal.t(FILTERED_ANNOUNCEMENT)
           );
