47243cb284848595e070906b615d54a9af8bf35a
Jes Constantine <jesconstantine@gmail.com>
2016-12-20 10:06:02 -0500

    Begin implement updated search results styles

--- web/modules/custom/mass_search/js/mass_search.results.js
+++ web/modules/custom/mass_search/js/mass_search.results.js
@@ -174,7 +174,7 @@
 
         // Prepend first promoted result with visually hidden <h3> landmark.
         var promotedResultsHeading = document.createElement('h3');
-        promotedResultsHeading.setAttribute('class', 'visually-hidden');
+        promotedResultsHeading.setAttribute('class', 'ma__sidebar-heading');
         promotedResultsHeading.textContent = 'Suggestions';
         searchResults.insertBefore(promotedResultsHeading, promotions[0]); // 5.
       }
@@ -200,7 +200,7 @@
       var regularResults = searchResults.querySelector('div.gsc-webResult.gsc-result:not(.gsc-promotion)');
       // Prepend regular results with visually hidden <h3> landmark.
       var regularResultsHeading = document.createElement('h3');
-      regularResultsHeading.setAttribute('class', 'visually-hidden');
+      regularResultsHeading.setAttribute('class', 'ma__sidebar-heading');
       regularResultsHeading.textContent = 'Results';
       searchResults.insertBefore(regularResultsHeading, regularResults); // 6.
     }
