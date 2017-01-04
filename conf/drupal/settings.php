f93ac0f387e541a5e4c1545bb6a2839a58553e05
Claudio Patrone <cpatrone@users.noreply.github.com>
2017-01-04 19:16:50 -0300

    MGRP-264: Edit Domain and remove Restrict by IP module (#306)
    This adds IP filtering/whitelisting for the edit domain and forces all secure drupal paths to return an error unless they are accessed via edit.mass.gov.

--- conf/drupal/settings.php
+++ conf/drupal/settings.php
@@ -20,3 +20,8 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 $settings['file_public_path'] = '${drupal.settings.file_public_path}';
 $settings['file_private_path'] = '${drupal.settings.file_private_path}';
+
+// Set variable to indicate if we are in "Edit Domain".
+// @see https://kbv2.acquia.com/articles/best-practices-setting-edit-domain
+$forwarded_host = (empty($_SERVER['HTTP_X_FORWARDED_HOST'])) ? $_SERVER['HTTP_HOST'] : $_SERVER['HTTP_X_FORWARDED_HOST'];
+$config['is_edit_domain'] = (strpos($forwarded_host, 'edit') !== FALSE) ? TRUE : FALSE;
