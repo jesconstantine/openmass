ff12c18088303f58dd43230d9ce86d6d5bd736f8
Giles Birch <gbirch@tech-tamer.com>
2017-03-16 15:30:32 -0400

    Massgov 1257 drupal 8 2 7 (#561)
    * Update Drupal core to 8.2.7, make conforming changes to update.php, settings and development.services.yml
    
    * Fix timestamps in composer.lock file

--- conf/drupal/settings.php
+++ conf/drupal/settings.php
@@ -16,7 +16,7 @@ $config_directories[CONFIG_SYNC_DIRECTORY] = '${drupal.config_sync_directory}';
 
 $settings['hash_salt'] = '${drupal.hash_salt}';
 $settings['update_free_access'] = FALSE;
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 $settings['file_public_path'] = '${drupal.settings.file_public_path}';
 $settings['file_private_path'] = '${drupal.settings.file_private_path}';
